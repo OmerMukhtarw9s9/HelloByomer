3b4e6492d9e52af443df9f6f1cd6738fe1994250
OmerMukhtarw9s9 <86936535+OmerMukhtarw9s9@users.noreply.github.com>
2021-07-04 22:02:03 -0400

    Changed to Hello world to Universe!

--- HelloByomer/HelloByomer.cpp
+++ HelloByomer/HelloByomer.cpp
@@ -1,11 +1,11 @@
 // HelloByomer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// july 4 2021
+// Omer Mukhtar
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello Universe!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
